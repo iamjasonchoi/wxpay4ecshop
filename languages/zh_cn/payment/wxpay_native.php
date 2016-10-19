5c4042242b314524d89153eae737265de4f865a5
iamjasonchoi <iamjasonchoi@qq.com>
2016-10-19 16:43:37 +0800

    Create wxpay_native.php

---
+++ languages/zh_cn/payment/wxpay_native.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * ECSHOP 微信支付语言文件
+ * ============================================================================
+ * * 版权所有 2005-2012 上海商派网络科技有限公司，并保留所有权利。
+ * 网站地址: http://www.ecshop.com；
+ * ----------------------------------------------------------------------------
+ * 这不是一个自由软件！您只能在不用于商业目的的前提下对程序代码进行修改和
+ * 使用；不允许对程序代码以任何形式任何目的的再发布。
+ * ============================================================================
+ * $Author: Jascon.C $
+ * $Id: bank.php 17217 2011-01-19 06:29:08Z liubo $
+ */
+
+
+$_LANG['wxpay_native'] = '微信扫码支付';
+$_LANG['wxpay_native_desc'] = '本支付适用于新版本手机微信扫二维码支付';
+$_LANG['wxpay_native_appid'] = '微信公众号AppId';
+$_LANG['wxpay_native_mchid'] = '商户号ID';
+$_LANG['wxpay_native_key'] = '商户支付密钥Key';
+$_LANG['wxpay_native_appsecret'] = '微信公众号AppSecret';
+$_LANG['wxpay_native_logs'] = 'logs';
+$_LANG['wxpay_native_signtype'] = '签名方式';
+$_LANG['wxpay_native_button'] = '立即用微信支付';
+$_LANG['wxpay_native_transaction_id'] = '交易单号';
+?>
